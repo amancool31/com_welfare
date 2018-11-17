33402834d914008270ca6795d1165f6f4b519ef8
amancool31 <amandeepsinghmadan@yahoo.com>
2018-11-18 03:52:40 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-particles-js';
+import { Button, Segment } from 'semantic-ui-react';
+import { Link ,Switch,BrowserRouter,Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+            <div className="App">
+      <Segment inverted>
+      <Link to="/map"><Button inverted color='teal'>
+        Map
+      </Button></Link>
+      <Link to="/register">
+      <Button inverted color='teal'>
+        Register 
+      </Button></Link>
+      <Link to="/feedback">
+      <Button inverted color='teal'>
+        FeedBack
+      </Button>
+      </Link>
+      <Link to="/contact">
+      <Button inverted color='teal'>
+        Contact us
+      </Button>
+      </Link>
+      </Segment>
+        <Particles params={{
+            		particles: {
+                  interactivity:{
+                    detect_on:"canvas",
+                    events:
+                    {onhover:{enable:true,mode:"repulse"},onclick:{enable:true,mode:"push"},resize:true},modes:{grab:{distance:400}}},
+            			line_linked: {
+            				shadow: {
+            					enable: true,
+            					color: "#3CA9D1",
+            					//blur: 3
+            				}
+            			}
+            		}
+            	}}
+              style={{
+                width: '100%',
+                //backgroundImage: `url(${a})` ,
+                background: "#6778c8",
+              }}/>
+               
+              <Switch>
+              <Route path='/register' />
+              <Route path='/contact'/>
+              <Route path='/feedback'/>
+              <Route path='/map'/>
+              </Switch>
+             
+              </div>
+
+      </BrowserRouter>
+      
     );
   }
 }
---
+++ src/register.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class register extends Component {
+  render() {
+    return (
+      <div>
+          
+
+
+      </div>
+    )
+  }
+}
